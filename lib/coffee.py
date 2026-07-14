58f0d50399d36c36f3ad70daf7807be3ba57c8ef
Calmax Omondi <calmax.omondi@student.moringaschool.com>
2026-07-14 10:22:39 +0300

    added tips

--- lib/coffee.py
+++ lib/coffee.py
@@ -12,3 +12,22 @@ class Coffee:
         else: 
             print("Size must be Small, Medium, or Large")
     print(f"You selected: {size}")
+
+    while True:
+        try:
+           price = int(input("Enter coffee price: "))
+           break
+        except ValueError:
+           print("Coffee price must be in numbers!")
+    
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
+    def display(self):
+        print(f"Size: {self.size}")
+        print(f"Price: {self.price}")
+    
+coffee1 = Coffee("small", 50)
+
+coffee1.tip()
+coffee1.display()
