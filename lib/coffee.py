365a021b3c82a581486d6a6564403b26f349911a
Calmax Omondi <calmax.omondi@student.moringaschool.com>
2026-07-14 09:54:50 +0300

    Added tip

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+    
+    while True:
+        size = input("Enter size(small, medium, or large): ").lower()
+        if size in ("small", "medium", "large"):
+            break
+        else: 
+            print("Size must be Small, Medium, or Large")
+    print(f"You selected: {size}")
