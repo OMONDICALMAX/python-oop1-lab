339f83c117cc625a5088843e7a3d514a8f58fc11
Calmax Omondi <calmax.omondi@student.moringaschool.com>
2026-07-14 08:38:29 +0300

    Completed lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,19 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+
+title = input("What is the title of the book you want to read today? ")
+
+while True:
+    try:
+        page_count = int(input("What page do you want to jump to? "))
+        break
+    except ValueError:
+        print("Page count must be an integer!")
+
+book1 = Book(title, page_count)
+
+print(book1.title)
+print(book1.page_count)
